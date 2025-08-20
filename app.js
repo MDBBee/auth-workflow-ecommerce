fca48bdc87f160b6b5ef6bc7f84a363b17f78b3e
MDBBee <bobbyugbebor@gmail.com>
2025-08-20 19:53:26 +0300

    Verification contrl1

--- app.js
+++ app.js
@@ -56,6 +56,7 @@ app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5000;
+
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URL);
--- controllers/authController.js
+++ controllers/authController.js
@@ -16,7 +16,7 @@ const register = async (req, res) => {
   const isFirstAccount = (await User.countDocuments({})) === 0;
   const role = isFirstAccount ? 'admin' : 'user';
 
-  const verificationToken = crypto.randomBytes(40).toString();
+  const verificationToken = crypto.randomBytes(40).toString('hex');
 
   const user = await User.create({
     name,
@@ -33,6 +33,24 @@ const register = async (req, res) => {
 };
 
 // 2)
+const verifyEmail = async (req, res) => {
+  const { verificationToken, email } = req.body;
+  const user = await User.findOne({ email });
+
+  if (!user) throw new CustomError.UnauthenticatedError('Verification failed');
+  if (user.verificationToken !== verificationToken)
+    throw new CustomError.UnauthenticatedError('Verification failed');
+
+  user.isVerified = true;
+  user.verified = Date.now();
+  user.verificationToken = '';
+
+  await user.save();
+
+  res.status(StatusCodes.OK).json({ msg: 'Email verified' });
+};
+
+// 3)
 const login = async (req, res) => {
   const { email, password } = req.body;
 
@@ -73,4 +91,5 @@ module.exports = {
   register,
   login,
   logout,
+  verifyEmail,
 };
